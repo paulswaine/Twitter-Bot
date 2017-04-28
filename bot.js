083683edd7bd559a2b025c560af6831186655d97
paulswaine <18swaine.paul@providencehigh.org>
2017-04-28 10:05:58 -0700

    uploaded image function
    we can now upload images

--- bot.js
+++ bot.js
@@ -121,12 +121,47 @@ function tweetIt() {
 //
 //
 //}
-//
-//
-//
-//
-//
-//
-//
-//
-//
+
+var fs = require('fs');
+processing();
+
+function processing(){
+    console.log("uploaded image");
+    var filename = 'pictures/image1.jpg';
+    
+    var parameters = {
+        encoding: 'base64'
+        
+    }
+    
+    var b64 = fs.readFileSync(filename, parameters);
+    
+    T.post('media/upload', {media_data: b64}, uploaded);
+    
+    function uploaded(err, data, response){
+        var id = data.media_id_string;
+        var tweet = {
+            
+            status: '#ROFL #LOL xD',
+            media_ids: [id]
+            
+        }
+        
+        T,post('statuses/update', tweet, tweeted);
+        
+        function tweeted(err, data, response){
+            if (err){
+                console.log("Something went wrong!");
+            }else{
+                console.log("It posted!");
+            }
+            
+        }
+        
+    }
+    
+    
+}
+
+
+
